cf923f8cb6c55e05fcaacc4ea0c531c64d4ac4d5
Ahumada04 <ahumadae@uci.edu>
2025-03-15 03:20:19 +0000

    big bangs

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,24 +1,55 @@
 #include "dijkstras.h"
-
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    int numV = G.size();
-    // INF being largest possible
-    // sub for actual vals
-    vector<int> distances(numV, INF);
-    // vector<bool> visited{numV, false};
-    // vector<int> distances[source] = 0;
-    // vector<int> previous[source] = 0; // UNDEFINED
-    // priority_queue<  
-    return {};
-};
+    int numVertices = G.size();
+    vector<int> distances(numVertices, INF);
+    vector<bool> visited(numVertices, false);
 
+    distances[source] = 0;
+    previous.assign(numVertices, UNDEFINED);
 
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+    minHeap.push({0, source});
+
+    while (!minHeap.empty()) {
+        int u = minHeap.top().second;
+        minHeap.pop();
+
+        if (visited[u]) continue;
+        visited[u] = true;
+
+        for (const Edge& edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+
+            if (!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push({distances[v], v});
+            }
+        }
+    }
+
+    return distances;
+}
 
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
-    return {};
+    vector<int> path;
+    if (distances[destination] == INF) {
+        return path;
+    }
+
+    for (int at = destination; at != UNDEFINED; at = previous[at]) {
+        path.push_back(at);
+    }
+    reverse(path.begin(), path.end());
+    return path;
 }
 
 void print_path(const vector<int>& v, int total) {
-    return;
+    for (int node : v) {
+        cout << node << " ";
+    }
+    cout << endl;
+    cout << "Total cost is " << total << endl;;
 }
