377f1bb07074083fda6c46ab852b537a499b2e10
Ahumada04 <ahumadae@uci.edu>
2025-03-15 03:41:08 +0000

    im gay

--- src/ladder.h
+++ src/ladder.h
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+
+
 void error(string word1, string word2, string msg);
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
 bool is_adjacent(const string& word1, const string& word2);
